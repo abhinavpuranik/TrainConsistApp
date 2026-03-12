2cea8ee1258d60cbf28c52e7fca7c5fee38f2bda
abhinavpuranik <abhunav.puranik@gmail.com>
2026-03-12 22:52:39 +0530

    Coded Use case nineteen to search for a bogie using binary search

--- src/com/trainconsist/main/TrainConsistApp.java
+++ src/com/trainconsist/main/TrainConsistApp.java
@@ -1,8 +1,8 @@
 /*
  * @author Developer
- * @version 13.0
+ * @version 14.0
  *
- * UC18: Linear Search for Bogie ID
+ * UC19: Binary Search for Bogie ID
  */
 
 package com.trainconsist.main;
@@ -14,46 +14,55 @@ public class TrainConsistApp {
     public static void main(String[] args) {
 
         System.out.println("====================================================");
-        System.out.println("UC18: Linear Search for Bogie ID");
+        System.out.println("UC19: Binary Search for Bogie ID");
         System.out.println("====================================================");
 
-        // Array of Bogie IDs (unsorted)
+        // Sorted array of bogie IDs
         String[] bogieIds = {
                 "BG101",
-                "BG305",
+                "BG125",
                 "BG210",
-                "BG450",
-                "BG125"
+                "BG305",
+                "BG450"
         };
 
-        for (int i = 0; i < bogieIds.length; i++) {
-
-            System.out.println(bogieIds[i]);
-        }
         
-        String searchKey = "BG450";
+        String key = "BG305";
 
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
         int position = -1;
 
-        // Linear Search
-        for (int i = 0; i < bogieIds.length; i++) {
+        // Binary Search
+        while (low <= high) {
 
-            if (bogieIds[i].equals(searchKey)) {
+            int mid = (low + high) / 2;
+
+            int comparison = key.compareTo(bogieIds[mid]);
+
+            if (comparison == 0) {
                 found = true;
-                position = i;
-                break; 
+                position = mid;
+                break;
+            } 
+            else if (comparison < 0) {
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
             }
         }
 
+       
         if (found) {
             System.out.println("Bogie found at position: " + position);
         } else {
             System.out.println("Bogie ID not found in the train consist.");
         }
 
-        System.out.println("UC18 linear search completed.");
+        System.out.println("UC19 binary search completed.");
 
-       
+      
     }
 }
